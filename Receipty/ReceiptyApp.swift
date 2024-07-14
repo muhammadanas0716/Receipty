df3790a4ce677a6fa2f89f11a93cf5c22ce20925
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-07-14 12:13:57 +0400

    Initial Commit

---
+++ Receipty/ReceiptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptyApp.swift
+//  Receipty
+//
+//  Created by Muhammad Anas on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
